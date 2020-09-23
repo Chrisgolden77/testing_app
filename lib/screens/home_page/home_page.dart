f7ca4de218f1e491cb6e3989826fe94425994db0
Chris Golden <chrgolden@gmail.com>
2020-09-22 17:45:45 -0700

    passing title properly

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -5,25 +5,18 @@ import 'package:flutter/material.dart';
 import '../../widgets/image_banner.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaningflutter up
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
+  MyHomePage({Key key, this.title}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(title);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _title = '';
+  _MyHomePageState(this._title);
 
   void _decrementCounter() {
     setState(() {
@@ -50,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.deepPurple,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("Count'em if you got'em!"),
+        title: Text(_title),
       ),
       body: Column(
         children: <Widget>[
